31994dcb9ec7a76d2311f084e6f11cad111c48fb
Benjamin Cai <benjamincaiyh@gmail.com>
2020-04-03 09:29:58 +0800

    Add sleeping control

--- get_data.py
+++ get_data.py
@@ -14,6 +14,9 @@ SCRAPED_DATA_PATH = os.getenv('SCRAPED_DATA_PATH')
 
 DATA_PATH = os.path.join(os.curdir, DATA_STORAGE_PATH, SCRAPED_DATA_PATH) # Ensure the path format is cross-platform compatible
 
+WAIT_REQUEST = 5
+WAIT_SECONDS = 3
+
 def get_challenges(amount=500, start_offset=0):
     """ Get the gievn `amount` of challenges from API, with `limit` of records 
         per request.
@@ -40,8 +43,9 @@ def get_challenges(amount=500, start_offset=0):
         else:
             print(f'Fethcing failed with status code: {res.status_code}')
 
-        if idx % 20 == 0:
-            time.sleep(3)
+        if idx % WAIT_REQUEST == 0:
+                print(f'Sleeping for {WAIT_SECONDS} seconds every {WAIT_REQUEST} requests...')
+                time.sleep(WAIT_SECONDS)
 
     concat_json_files(DATA_PATH, 'challenges_overview_*.json', 'challenges_overview.json')
 
@@ -68,8 +72,9 @@ def get_challenge_detail():
             else:
                 print(f'Fetching failed with status code: {res.status_code}')
 
-            if idx % 20 == 0:
-                time.sleep(3)
+            if idx % WAIT_REQUEST == 0:
+                print(f'Sleeping for {WAIT_SECONDS} seconds every {WAIT_REQUEST} requests...')
+                time.sleep(WAIT_SECONDS)
 
     concat_json_files(DATA_PATH, 'challenges_detail_*.json', 'challenges_detail.json')
 
@@ -115,13 +120,13 @@ def get_users():
             else:
                 print(f'Fetching user profiled failed: {handle} | {start_idx + idx}/{len(registrant_handles)}')
 
-            if idx % 20 == 0:
-                print('Sleeping for 3 seconds every 20 requests...')
-                time.sleep(3)
+            if idx % WAIT_REQUEST == 0:
+                print(f'Sleeping for {WAIT_SECONDS} seconds every {WAIT_REQUEST} requests...')
+                time.sleep(WAIT_SECONDS)
 
     concat_json_files(DATA_PATH, 'users_profile_*.json', 'users_profile.json')
 
 if __name__ == '__main__':
     # get_challenges(amount=500)
-    # get_challenge_detail()
+    get_challenge_detail()
     get_users()
